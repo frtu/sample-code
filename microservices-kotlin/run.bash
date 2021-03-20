168b5dca08f014abdb27d0daade5204123e1f78d
Tu, Fred <frederic.tu@gmail.com>
2021-03-20 21:34:24 +0800

    Create additional level for multiple project

--- microservices-kotlin/run.bash
+++ microservices-kotlin/run.bash
@@ -1,13 +1,13 @@
-echo "-> Run bash individually - Type 'microservice-kotlinbuild' to build the project"
-microservice-kotlinbuild() {
+echo "-> Run bash individually - Type 'microservices-kotlinbuild' to build the project"
+microservices-kotlinbuild() {
   echo "mvn clean package"
   mvn clean package
 }
-echo "-> Run bash individually - Type 'microservice-kotlinrun' to run the executable"
-microservice-kotlinrun() {
+echo "-> Run bash individually - Type 'microservices-kotlinrun' to run the executable"
+microservices-kotlinrun() {
   echo "Running executable > mvn spring-boot:run"
   mvn spring-boot:run
 }
 
-microservice-kotlinbuild
-microservice-kotlinrun
+microservices-kotlinbuild
+microservices-kotlinrun
