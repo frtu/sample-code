d0ba3e61e0ce7c23314e93222432d48a2b0daae1
Tu, Fred <frederic.tu@gmail.com>
2021-05-05 00:12:48 +0800

    Add logger to UT for troubleshooting

--- persistence-r2dbc/src/test/kotlin/com/github/frtu/persistence/r2dbc/entitytemplate/EmailRepositoryTest.kt
+++ persistence-r2dbc/src/test/kotlin/com/github/frtu/persistence/r2dbc/entitytemplate/EmailRepositoryTest.kt
@@ -7,10 +7,14 @@ import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
 import reactor.core.publisher.Flux
 
 internal class EmailRepositoryTest {
+    private val LOGGER: Logger = LoggerFactory.getLogger(EmailRepositoryTest::class.java)
+
     @Test
     fun `Test basic findAll`() {
         val email1 = Email("rndfred@gmail.com", "Mail subject", "Lorem ipsum dolor sit amet.", "INIT")
@@ -22,9 +26,11 @@ internal class EmailRepositoryTest {
         } returns Flux.just(email1, email2)
 
         val repository = EmailRepository(template)
+        LOGGER.debug("R2dbcEntityTemplate: ${template}")
         runBlocking {
             // Execute flow to list
             val result = repository.findAll().toList(mutableListOf())
+            LOGGER.debug("Test result: ${result}")
             assertThat(result).isNotNull()
             assertThat(result[0]).isEqualTo(email1)
             assertThat(result[1]).isEqualTo(email2)
