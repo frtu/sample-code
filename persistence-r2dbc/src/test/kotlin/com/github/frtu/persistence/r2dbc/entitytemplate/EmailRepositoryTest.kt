02d2b5ff3806966fc98d803050f7a9978b2cbfc0
Tu, Fred <frederic.tu@gmail.com>
2021-05-05 00:05:23 +0800

    Add finer grain test

--- persistence-r2dbc/src/test/kotlin/com/github/frtu/persistence/r2dbc/entitytemplate/EmailRepositoryTest.kt
+++ persistence-r2dbc/src/test/kotlin/com/github/frtu/persistence/r2dbc/entitytemplate/EmailRepositoryTest.kt
@@ -13,17 +13,21 @@ import reactor.core.publisher.Flux
 internal class EmailRepositoryTest {
     @Test
     fun `Test basic findAll`() {
+        val email1 = Email("rndfred@gmail.com", "Mail subject", "Lorem ipsum dolor sit amet.", "INIT")
+        val email2 = Email("rndfred@hotmail.com", "Mail subject", "Lorem ipsum dolor sit amet.", "SENT")
+
         val template = mockk<R2dbcEntityTemplate>()
         every {
             template.select(Email::class.java).all()
-        } returns Flux.just(
-            Email("rndfred@gmail.com", "Mail subject", "Lorem ipsum dolor sit amet.", "SENT")
-        )
+        } returns Flux.just(email1, email2)
 
         val repository = EmailRepository(template)
         runBlocking {
+            // Execute flow to list
             val result = repository.findAll().toList(mutableListOf())
             assertThat(result).isNotNull()
+            assertThat(result[0]).isEqualTo(email1)
+            assertThat(result[1]).isEqualTo(email2)
         }
     }
 }
