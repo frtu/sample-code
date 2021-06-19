48257f80aa133dd89651f04f6fe754676c345202
Tu, Fred <frederic.tu@gmail.com>
2021-06-19 10:43:30 +0800

    refactor(docker) : move postgres docker into subfolder /storage => Add one more layer

--- event-notification-mails/scripts.bash
+++ event-notification-mails/scripts.bash
@@ -3,14 +3,14 @@ CONTAINER_NAME='postgres_database_1'
 echo "Type 'startpostgres' to start postgres server"
 startpostgres() {
   echo "Make sure you have ** docker-compose ** installed !!"
-  (cd docker/ && exec docker-compose up)
+  (cd docker/storage/ && exec docker-compose up)
 }
 echo "Type 'postgresrm' to delete docker image & remove data"
 postgresrm() {
     echo "docker rm ${CONTAINER_NAME}"
     docker rm ${CONTAINER_NAME}
 
-    rm -Rf docker/db/data/
+    rm -Rf docker/storage/db/data/
 }
 
 startpostgres
