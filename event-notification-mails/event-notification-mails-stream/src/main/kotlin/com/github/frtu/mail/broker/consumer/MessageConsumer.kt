5d1f645dab2bdbd56c2a083bc88d91b350ababd9
Tu, Fred <frederic.tu@gmail.com>
2021-07-04 23:39:43 +0800

    Enable useNativeDecoding=true & use logger instead of print

--- event-notification-mails/event-notification-mails-stream/src/main/kotlin/com/github/frtu/mail/broker/consumer/MessageConsumer.kt
+++ event-notification-mails/event-notification-mails-stream/src/main/kotlin/com/github/frtu/mail/broker/consumer/MessageConsumer.kt
@@ -2,6 +2,7 @@ package com.github.frtu.mail.broker.consumer
 
 import com.github.frtu.mail.broker.channel.EmailResultChannel
 import com.github.frtu.mail.broker.channel.EmailSourceChannel
+import org.slf4j.LoggerFactory
 import org.springframework.cloud.stream.annotation.EnableBinding
 import org.springframework.cloud.stream.annotation.StreamListener
 import org.springframework.stereotype.Service
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Service
 class MessageConsumer {
     @StreamListener(EmailSourceChannel.EMAIL_SOURCE)
     fun handle(message: String) {
-        println("Received: $message")
+        logger.info("Received: $message type:${message.javaClass}")
     }
+
+    internal val logger = LoggerFactory.getLogger(this::class.java)
 }
