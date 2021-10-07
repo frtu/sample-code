ee292c8f0575f9601a3b846a2c1173d2c8bfb03a
Tu, Fred <frederic.tu@gmail.com>
2021-10-07 23:58:35 +0800

    Reformat Kafka consumer

--- spring-integration-kotlin/spring-integration-kotlin-stream/src/main/kotlin/com/github/frtu/sample/integration/config/KafkaConfiguration.kt
+++ spring-integration-kotlin/spring-integration-kotlin-stream/src/main/kotlin/com/github/frtu/sample/integration/config/KafkaConfiguration.kt
@@ -4,43 +4,40 @@ import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
-import org.springframework.integration.annotation.ServiceActivator
-
-import org.springframework.integration.kafka.inbound.KafkaMessageDrivenChannelAdapter.ListenerMode
-
-import org.springframework.integration.dsl.IntegrationFlows
-
 import org.springframework.integration.dsl.IntegrationFlow
+import org.springframework.integration.dsl.IntegrationFlows
 import org.springframework.integration.kafka.dsl.Kafka
+import org.springframework.integration.kafka.inbound.KafkaMessageDrivenChannelAdapter.ListenerMode
 import org.springframework.kafka.core.ConsumerFactory
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
 import org.springframework.kafka.listener.ContainerProperties
 import org.springframework.kafka.listener.KafkaMessageListenerContainer
-import org.springframework.messaging.Message
-import java.lang.Exception
 
 @Configuration
+@ComponentScan("com.github.frtu.sample.integration.service")
 class KafkaConfiguration {
     @Value("\${spring.kafka.bootstrap-servers}")
     private val bootstrapServers: String? = null
 
     @Value("\${application.topic.integration-source}")
-    private val springIntegrationKafkaTopic: String? = null
+    private val inputTopic: String? = null
 
     @Bean
     @Throws(Exception::class)
-    fun kafkaReader(): IntegrationFlow? {
-        return IntegrationFlows
-            .from(Kafka.messageDrivenChannelAdapter(listener(), ListenerMode.record))
-            .channel("kafka-reader")
+    fun kafkaReader(): IntegrationFlow =
+        IntegrationFlows
+            .from(
+                Kafka.messageDrivenChannelAdapter(
+                    KafkaMessageListenerContainer(
+                        consumerFactory(),
+                        ContainerProperties(inputTopic)
+                    ), ListenerMode.record
+                )
+            )
+            .channel(KAFKA_INPUT)
             .get()
-    }
-
-    @Bean
-    fun listener(): KafkaMessageListenerContainer<String, String> {
-        return KafkaMessageListenerContainer(consumerFactory(), ContainerProperties(springIntegrationKafkaTopic))
-    }
 
     @Bean
     fun consumerFactory(): ConsumerFactory<String, String> {
@@ -52,9 +49,7 @@ class KafkaConfiguration {
         return DefaultKafkaConsumerFactory(props)
     }
 
-
-    @ServiceActivator(inputChannel = "kafka-reader")
-    fun Print(msg: Message<*>) {
-        System.out.println(msg.getPayload().toString())
+    companion object {
+        const val KAFKA_INPUT = "kafka-input"
     }
 }
