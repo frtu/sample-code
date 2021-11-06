b21b172d95254aedb5d7a3e0965f7c042a6beefd
Tu, Fred <frederic.tu@gmail.com>
2021-11-07 01:14:05 +0800

    Adding more cases for First

--- coroutine-core/src/main/kotlin/com/github/frtu/samples/coroutine/core/First.kt
+++ coroutine-core/src/main/kotlin/com/github/frtu/samples/coroutine/core/First.kt
@@ -1,10 +1,12 @@
 package com.github.frtu.samples.coroutine.core
 
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.flowOf
 
 suspend fun main(args: Array<String>) {
     val flowOf = flowOf(4, 5, 6)
-    val firstOrNull = flowOf.firstOrNull()
-    println(firstOrNull)
+    println(flowOf.first())
+    println(flowOf.first { it > 5 })
+    println(flowOf.firstOrNull { it > 10 })
 }
