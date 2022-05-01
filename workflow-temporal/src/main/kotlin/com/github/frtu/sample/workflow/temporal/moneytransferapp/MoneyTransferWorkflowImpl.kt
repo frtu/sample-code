f08549fcca8a15224b6c995d2b9e0370a816dbee
Tu, Fred <frederic.tu@gmail.com>
2022-05-01 19:03:21 +0800

    temporal : use Kotlin-DSL

--- workflow-temporal/src/main/kotlin/com/github/frtu/sample/workflow/temporal/moneytransferapp/MoneyTransferWorkflowImpl.kt
+++ workflow-temporal/src/main/kotlin/com/github/frtu/sample/workflow/temporal/moneytransferapp/MoneyTransferWorkflowImpl.kt
@@ -8,24 +8,25 @@ import java.util.*
 
 class MoneyTransferWorkflowImpl : MoneyTransferWorkflow {
     // RetryOptions specify how to automatically handle retries when Activities fail.
-    private val retryOptions = RetryOptions.newBuilder()
-        .setInitialInterval(Duration.ofSeconds(1))
-        .setMaximumInterval(Duration.ofSeconds(100))
-        .setBackoffCoefficient(2.0)
-        .setMaximumAttempts(500)
-        .build()
+    private val retryOptions = RetryOptions {
+        setInitialInterval(Duration.ofMillis(100))
+        setMaximumInterval(Duration.ofSeconds(10))
+        setBackoffCoefficient(2.0)
+        setMaximumAttempts(10)
+    }
 
-    private val defaultActivityOptions =
-        ActivityOptions.newBuilder() // Timeout options specify when to automatically timeout Activities if the process is taking too long.
-            .setStartToCloseTimeout(Duration.ofSeconds(5)) // Optionally provide customized RetryOptions.
-            // Temporal retries failures by default, this is simply an example.
-            .setRetryOptions(retryOptions)
-            .build()
+    private val defaultActivityOptions = ActivityOptions {
+        // ActivityOptions DSL
+        setTaskQueue("TestQueue")
+        setStartToCloseTimeout(Duration.ofSeconds(5)) // Timeout options specify when to automatically timeout Activities if the process is taking too long.
+        // Temporal retries failures by default, this is simply an example.
+        setRetryOptions(retryOptions)
+    }
 
     // ActivityStubs enable calls to methods as if the Activity object is local, but actually perform an RPC.
-    private val perActivityMethodOptions: Map<String, ActivityOptions> = HashMap<String, ActivityOptions>().apply {
-        put(WITHDRAW, ActivityOptions.newBuilder().setHeartbeatTimeout(Duration.ofSeconds(5)).build())
-    }
+    private val perActivityMethodOptions: Map<String, ActivityOptions> = mapOf<String, ActivityOptions>(
+        WITHDRAW to ActivityOptions { setHeartbeatTimeout(Duration.ofSeconds(5)) }
+    )
 
     private val account =
         Workflow.newActivityStub(AccountActivity::class.java, defaultActivityOptions, perActivityMethodOptions)
