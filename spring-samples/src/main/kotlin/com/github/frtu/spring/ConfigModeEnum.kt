d04a4f75e5da21a50e64fe689668fdd415200250
Tu, Fred <frederic.tu@gmail.com>
2022-12-18 23:24:26 +0800

    feat: bump version for spring-samples

--- spring-samples/src/main/kotlin/com/github/frtu/spring/ConfigModeEnum.kt
+++ spring-samples/src/main/kotlin/com/github/frtu/spring/ConfigModeEnum.kt
@@ -4,6 +4,6 @@ enum class ConfigModeEnum {
     DEFAULT_VALUE, FILE_VALUE, TEST_OVERRIDE_VALUE, ANNOTATION_OVERRIDE_VALUE;
 
     companion object {
-        fun getConfigModeByName(name: String) = valueOf(name.toUpperCase())
+        fun getConfigModeByName(name: String) = valueOf(name.uppercase())
     }
 }
