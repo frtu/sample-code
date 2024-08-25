b8a5d6770cf1f6cdb0e2cd757b30771bad3bfb1a
Tu, Fred <frederic.tu@gmail.com>
2024-08-25 20:25:21 +0800

    refactor: focus on slack bolt socket mode

---
+++ bot-slack/README.md
@@ -0,0 +1,26 @@
+# Project - bot-slack
+
+## About
+
+Allow to connect to Slack & receive message.
+
+## Enablement
+
+This project intend to use [socket mode](https://slack.dev/java-slack-sdk/guides/socket-mode) which rely on Websocket &
+avoid the need to open HTTP webhook callback.
+
+When creating and installing your app, don't forget to 
+
+* Go to Settings > Basic Information > Add a new App-Level Token with the `connections:write` scope
+* Get the generated token value that starts with xapp-
+
+* Go to Settings > Socket Mode > Turn on `Enable Socket Mode`
+* Configure the features (without setting Request URLs)
+
+* Install the app to receive bot/user tokens (bot: xoxb-, user: xoxp-)
+
+## Release notes
+
+### 0.0.1-SNAPSHOT - Current version
+
+* Initial version with Bolt in Socket Mode
--- bot-slack/src/main/kotlin/com/github/frtu/sample/bot/slack/Main.kt
+++ bot-slack/src/main/kotlin/com/github/frtu/sample/bot/slack/Main.kt
@@ -1,68 +1,52 @@
 package com.github.frtu.sample.bot.slack
 
-import com.slack.api.Slack
+import com.slack.api.app_backend.events.payload.EventsApiPayload
 import com.slack.api.bolt.App
 import com.slack.api.bolt.AppConfig
+import com.slack.api.bolt.context.builtin.EventContext
 import com.slack.api.bolt.socket_mode.SocketModeApp
-import com.slack.api.methods.MethodsClient
-import com.slack.api.methods.kotlin_extension.request.chat.blocks
+import com.slack.api.model.event.AppMentionEvent
+import com.slack.api.socket_mode.SocketModeClient
 
+/**
+ * https://slack.dev/java-slack-sdk/guides/socket-mode
+ * https://api.slack.com/apis/socket-mode
+ */
 fun main() {
     // Create a new Slack app
     val botToken = System.getenv("SLACK_BOT_TOKEN")
     val botSigningSecret = System.getenv("SLACK_BOT_SIGNING_SECRET")
-    val app = App(
-        AppConfig.builder()
-            .singleTeamBotToken(botToken)
-            .signingSecret(botSigningSecret)
-            .build()
-    )
+    val appConfig = AppConfig.builder()
+        .singleTeamBotToken(botToken)
+        .signingSecret(botSigningSecret)
+        .build()
 
-    // https://slack.dev/java-slack-sdk/guides/web-api-basics
-    val slack = Slack.getInstance()
+    // Initialize the App and register listeners
+    val app = App(appConfig)
 
-    // Initialize an API Methods client with the given token
-    val methods: MethodsClient = slack.methods(botToken)
-
-    // Build a request object
-    val response = methods.chatPostMessage { req ->
-        req
-            .channel("#random")
-            .blocks {
-                section {
-                    // "text" fields can be constructed via `plainText()` and `markdownText()`
-                    markdownText("*Please select a restaurant:*")
-                }
-                divider()
-                actions {
-                    // To align with the JSON structure, you could put the `elements { }` block around the buttons but for brevity it can be omitted
-                    // The same is true for things such as the section block's "accessory" container
-                    button {
-                        // For instances where only `plain_text` is acceptable, the field's name can be filled with `plain_text` inputs
-                        text("Farmhouse", emoji = true)
-                        value("v1")
-                    }
-                    button {
-                        text("Kin Khao", emoji = true)
-                        value("v2")
-                    }
-                }
-            }
+    app.event(AppMentionEvent::class.java) { req: EventsApiPayload<AppMentionEvent>, ctx: EventContext ->
+        ctx.say("Hi there!")
+        ctx.ack()
     }
 
-    // Get a response as a Java object
-    if (response.isOk) {
-        val postedMessage = response.message
-        println(postedMessage)
-    } else {
-        val errorCode = response.error // e.g., "invalid_auth", "channel_not_found"
-    }
-
+    // -----------------------------------------------------
     // Start the app in Socket Mode
+    // https://slack.dev/java-slack-sdk/guides/socket-mode
+    // -----------------------------------------------------
+    // the app-level token with `connections:write` scope
     val appToken = System.getenv("SLACK_APP_TOKEN")
-    val socketModeApp = SocketModeApp(appToken, app)
+    // Initialize the adapter for Socket Mode
+    // with an app-level token and your Bolt app with listeners.
+    val socketModeApp = SocketModeApp(
+        appToken,
+        SocketModeClient.Backend.JavaWebSocket,
+        app,
+    )
+    // #start() method establishes a new WebSocket connection and then blocks the current thread.
+    // If you do not want to block this thread, use #startAsync() instead.
     socketModeApp.start()
 
+    // HTTP receiving message https://api.slack.com/tools/bolt-java
 //    val server = SlackAppServer(app)
 //    server.start() // http://localhost:3000/slack/events
 }
