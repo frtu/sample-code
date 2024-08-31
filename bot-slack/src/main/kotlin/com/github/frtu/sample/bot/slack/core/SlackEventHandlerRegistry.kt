f1674e09e89db05b4be25fd8d685b3686aa29dcb
Tu, Fred <frederic.tu@gmail.com>
2024-08-31 22:38:40 +0800

    feat: adding tests & logs

--- bot-slack/src/main/kotlin/com/github/frtu/sample/bot/slack/core/SlackEventHandlerRegistry.kt
+++ bot-slack/src/main/kotlin/com/github/frtu/sample/bot/slack/core/SlackEventHandlerRegistry.kt
@@ -1,15 +1,16 @@
 package com.github.frtu.sample.bot.slack.core
 
 import com.slack.api.bolt.handler.BoltEventHandler
-import com.slack.api.model.event.Event
 import jakarta.annotation.PostConstruct
-import kotlin.reflect.KClass
-import kotlin.reflect.KType
-import kotlin.reflect.javaType
-import kotlin.reflect.typeOf
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Repository
 
+/**
+ * Registry for all Event type class
+ *
+ * @see <a href="https://api.slack.com/events/message">Event type</a>
+ * @see <a href="https://oss.sonatype.org/service/local/repositories/releases/archive/com/slack/api/slack-api-model/1.42.0/slack-api-model-1.42.0-javadoc.jar/!/com/slack/api/model/event/Event.html">All Event types</a>
+ */
 @Repository
 class SlackEventHandlerRegistry(
     boltEventHandlers: List<Pair<Class<*>, BoltEventHandler<*>>> = emptyList()
